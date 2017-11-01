4d304fa1cacaa7efb35de394dcccf52940315c88
Ryan Blecher <notryanb@gmail.com>
2017-10-31 20:08:54 -0400

    Open window, but it immediately closes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate piston_window;
+extern crate opengl_graphics;
+extern crate rand;
+
+use piston_window::{
+    Button,
+    EventLoop,
+    Input,
+    Motion,
+    OpenGL,
+    PistonWindow,
+    WindowSettings
+};
+use opengl_graphics::GlGraphics;
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: u32 = 1024;
+    const HEIGHT: u32 = 640;
+
+    let opengl = OpenGL::V3_2;
+    // let game_size = Size::new(1024.0, 600.0);
+    let mut window: PistonWindow = WindowSettings::new(
+            "Disasteroids",
+            [WIDTH, HEIGHT]
+        )
+        .opengl(opengl)
+        .samples(8)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    window.set_ups(60);
+    window.set_max_fps(60);    
 }
